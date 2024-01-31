82bd407fef4dc00e8d2bced641bb6ad6f95cb34d
Kirtan Modi <modikirtan19@gmail.com>
2024-01-31 18:00:22 +0530

    changed label

---
+++ Udder/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Udder
+//
+//  Created by Kirtan Modi on 31/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Udder")
+                .font(.system(size: 40))
+            Text("Fresh Milk on demand")
+                .font(.system(size: 20))
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Udder/UdderApp.swift
@@ -0,0 +1,17 @@
+//
+//  UdderApp.swift
+//  Udder
+//
+//  Created by Kirtan Modi on 31/01/24.
+//
+
+import SwiftUI
+
+@main
+struct UdderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
